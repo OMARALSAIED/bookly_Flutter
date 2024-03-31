5fd4e2dbca843ad74aaf2af08d10022140b0dea8
omaralsaied <wwwomrtre@gmail.com>
2024-03-31 14:54:28 +0300

    spalsh view ui done

---
+++ lib/constanse.dart
@@ -0,0 +1,3 @@
+import 'dart:ui';
+
+const kprimaryColor= Color(0xff100B20);
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bookly/Features/splash/presentation/views/splash_view.dart';
+import 'package:bookly/constanse.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -11,8 +12,10 @@ class Bookly extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
-      home: SplashView(),
+    return  GetMaterialApp(
+      theme: ThemeData().copyWith(scaffoldBackgroundColor:kprimaryColor),
+      debugShowCheckedModeBanner: false,
+      home:const SplashView(),
     );
   }
 }
