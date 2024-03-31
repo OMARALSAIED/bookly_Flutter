186326d0330ce7fd9f40db329fe4e293630af602
omaralsaied <wwwomrtre@gmail.com>
2024-03-31 15:56:14 +0300

    adding sliding anmation to splash view

--- lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,9 +1,37 @@
+import 'package:bookly/Features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:bookly/core/utils/asstes.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  
+  @override
+  void initState() {
+    
+    super.initState();
+
+    animationController=AnimationController(vsync: this,duration: const Duration(seconds: 1));
+    slidingAnimation=Tween<Offset>(begin: const Offset(0, 2),end: Offset.zero).animate(animationController);
+    animationController.forward();
+    
+  }
+  @override
+  void dispose() {
+    // يقوم بغلق الكونترولر لغق الموارد
+    super.dispose();
+    animationController.dispose();
+  }
+
+  
   @override
   Widget build(BuildContext context) {
     return  Column(
@@ -13,8 +41,13 @@ class SplashViewBody extends StatelessWidget {
       
       Image.asset(AssetsData.logo),
       const SizedBox(height: 4,),
-     const Text("Read Free Books",textAlign: TextAlign.center,)
+      
+     SlidingText(slidingAnimation: slidingAnimation)
     ],
     );
   }
 }
+
+
+
+
