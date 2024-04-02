76e28fd7d6d257cb5c9b124333fa9821920a05ce
omaralsaied <wwwomrtre@gmail.com>
2024-04-02 13:24:50 +0300

    refactor intit State & and Navgiate to Home page

--- lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,6 +1,9 @@
+import 'package:bookly/Features/home/presentation/views/home_view.dart';
 import 'package:bookly/Features/splash/presentation/views/widgets/sliding_text.dart';
+import 'package:bookly/constanse.dart';
 import 'package:bookly/core/utils/asstes.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 
 class SplashViewBody extends StatefulWidget {
@@ -18,12 +21,14 @@ class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProvid
   void initState() {
     
     super.initState();
-
-    animationController=AnimationController(vsync: this,duration: const Duration(seconds: 1));
-    slidingAnimation=Tween<Offset>(begin: const Offset(0, 2),end: Offset.zero).animate(animationController);
-    animationController.forward();
+    initSlidinganiation();
+    navigateToHome();
+    
     
   }
+
+
+ 
   @override
   void dispose() {
     // يقوم بغلق الكونترولر لغق الموارد
@@ -31,7 +36,7 @@ class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProvid
     animationController.dispose();
   }
 
-  
+  //Build Method
   @override
   Widget build(BuildContext context) {
     return  Column(
@@ -45,6 +50,25 @@ class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProvid
      SlidingText(slidingAnimation: slidingAnimation)
     ],
     );
+  }
+
+  //Method To Navigateto Home with anmiation
+  void navigateToHome() {
+    
+    Future.delayed(const Duration(seconds: 2),() 
+    {
+      Get.to(()=>const Homeview(),transition: Transition.fade,duration: kTranstionDuartion);
+    });
+  }
+
+
+
+
+//Anmiation function To reuse 
+   void initSlidinganiation() {
+    animationController=AnimationController(vsync: this,duration: const Duration(seconds: 1));
+    slidingAnimation=Tween<Offset>(begin: const Offset(0, 2),end: Offset.zero).animate(animationController);
+    animationController.forward();
   }
 }
 
--- lib/constanse.dart
+++ lib/constanse.dart
@@ -1,3 +1,4 @@
 import 'dart:ui';
 
 const kprimaryColor= Color(0xff100B20);
+const kTranstionDuartion=Duration(milliseconds: 250);
