698df676a6ada948c2e4e37be4d3ac77c1223263
omaralsaied <wwwomrtre@gmail.com>
2024-04-03 12:04:52 +0300

    Create Book Deatiles View

---
+++ lib/Features/home/presentation/views/widgets/boo_deatiles_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BookDeatilesViewBody extends StatelessWidget {
+  const BookDeatilesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
