c008e7b48b56a6fe8db43543eed012f1ab945c91
omaralsaied <wwwomrtre@gmail.com>
2024-04-05 15:03:54 +0300

    Create  Failure class

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -1,5 +1,9 @@
+import 'package:bookly/Features/home/data/models/books_model/books_model.dart';
+import 'package:bookly/core/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo
 {
-  fetchBestSellerBooks();
-  fetchfeaturedBooks();
+Future<Either<Failure,List<BooksModel>>>  fetchBestSellerBooks();
+Future<Either<Failure,List<BooksModel>>>  fetchfeaturedBooks();
 }
